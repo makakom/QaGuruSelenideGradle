ad3d491724782d8b0659dd42953ba1b54738ff5e
makakom <max_pain@mail.ru>
2022-01-03 15:57:36 +0300

    update firstTest.java

--- src/test/java/firstTest.java
+++ src/test/java/firstTest.java
@@ -1,44 +1,38 @@
-import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class firstTest {
 
+    @Test
+    void searchExampleCodeForJUnit5() {
+        //Откройте страницу Selenide в Github
+        open("https://github.com/selenide/selenide");
+
+        //Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();
+
+        //Убедитесь, что в списке страниц (Pages) есть страница c
+        $("#wiki-pages-filter").val("SoftAssertions").pressEnter();
+
+        $("[data-filterable-for='wiki-pages-filter']").shouldHave(text("SoftAssertions"));
+
+        //Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $("[data-filterable-for='wiki-pages-filter']").$(byText("SoftAssertions")).click();
+
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+    }
 
     @Test
-    @DisplayName("Первый тест 10 поток")
-    public void firstTestQAGuru() {
-        open("https://demoqa.com/automation-practice-form");
-
-        $("#firstName").setValue("Ivan");
-        $("#lastName").setValue("Ivanov");
-        $("#userEmail").setValue("test@tset.ru");
-        $("#userNumber").setValue("89211111111");
-        $("#genterWrapper").$(byText("Male")).click();
-
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("December");
-        $(".react-datepicker__year-select").selectOption("1992");
-        $(".react-datepicker__month").$(byText("20")).click();
-
-        $("#subjectsInput").setValue("Economics").pressEnter();
-        $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/paintMaster.png"));
-        $("#currentAddress").setValue("test");
-        $("#react-select-3-input").setValue("NCR").pressEnter();
-        $("#react-select-4-input").setValue("Noida").pressEnter();
-        $("#submit").scrollTo().click();
-
-        //Asserts
-        $(".modal-content").shouldBe(Condition.visible);
-        $$x("//div[@class='modal-body']//td[2]").shouldHave(CollectionCondition.exactTexts(
-                "Ivan Ivanov", "test@tset.ru", "Male", "8921111111", "20 December,1992", "Economics",
-                "Sports", "paintMaster.png", "test", "NCR Noida"));
+    void testDragAndDrop() {
+        //Откройте https://the-internet.herokuapp.com/drag_and_drop
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        //Перенесите прямоугольник А на место В
+        $("#column-a").dragAndDropTo($("#column-b"));
+        //Проверьте, что прямоугольники действительно поменялись
+        $("#column-a").shouldHave(text("b"));
     }
 }
