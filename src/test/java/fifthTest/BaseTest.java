c37b6c27702cc8f4a5ce3f70bf18e04644e0888c
makakom <max_pain@mail.ru>
2022-01-30 14:09:18 +0300

    added Attach.java

--- src/test/java/fifthTest/BaseTest.java
+++ src/test/java/fifthTest/BaseTest.java
@@ -1,8 +1,10 @@
 package fifthTest;
 
 import com.codeborne.selenide.Configuration;
+import helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -10,12 +12,22 @@ public class BaseTest {
 
     @BeforeAll
     static void setup() {
-        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
     void afterEach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
         closeWebDriver();
     }
 }
